e7d3286d58c230adcbf3993552e111cf5b37decb
Ilija Mandic <imandic@devtonomy.net>
2021-04-19 00:12:51 +0200

    LuaWrapper Test

---
+++ LuaWrapperTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LuaWrapper;
+
+
+namespace LuaWrapperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr luaState = Lua.luaL_newstate();
+            Lua.luaL_openlibs(luaState);
+            string luaString = "print('Hello World');";
+            Lua.luaL_dostring(luaState, luaString);
+            Lua.lua_close(luaState);
+
+        }
+    }
+}
